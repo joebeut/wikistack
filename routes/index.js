04244b30a7ac787a129f9abdaf3045f9950baec8
Joe Beutler <jtb2133@columbia.edu>
2015-03-01 21:16:28 -0500

    completed initial layout structure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Browse My WikiStack', name: 'WikiStack', link: "Add Page", index_list: ["Fullstack Home", "About Me", "My Projects", "My City", "Code I've Written"], footer: "WikiStack, Fullstack Academy" });
 });
 
 module.exports = router;
