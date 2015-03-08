e7b8f65c1a13f60751eb299e6ac8617eed706e58
Joe Beutler <jtb2133@columbia.edu>
2015-03-08 14:51:21 -0400

    completed through markdown

--- filters.js
+++ filters.js
@@ -13,4 +13,11 @@ module.exports = function(swig) {
   page_link.safe = true;
 
   swig.setFilter('page_link', page_link);
+
+  var marked = require('marked');
+  var markedFilter = function (body) {
+    return marked(body);
+  };
+  markedFilter.safe = true;
+  swig.setFilter('marked', markedFilter);
 };
--- routes/index.js
+++ routes/index.js
@@ -17,27 +17,9 @@ router.get('/', function(req, res, next) {
 router.get('/wiki/:url_name', function(req, res) {
 	var url = req.params.url_name;
 
-	console.log('This is docs: ' + docs);
-
-	var mystring = JSON.stringify(docs);
-
-	var mydocs = JSON.parse(mystring);
-
-	console.log(mydocs);
-
-	for(doc in mydocs){
-		console.log("Doc title: " + doc.body);
-		for(var key in doc) {
-			var value = doc[key];
-			console.log("Key values: " + value);
-		}
-		
-	}
-
-
-	
-  	
-  	res.render( 'wiki', { title: doc, body: doc, name: 'WikiStack', link: "Add Page", footer: "WikiStack, Fullstack Academy"});
+	model.Page.findOne({url_name: url}, function (err, data){
+        res.render( 'wiki', { title: data.title, body: data.body, name: 'WikiStack', link: "Add Page", footer: "WikiStack, Fullstack Academy"});
+	  });
 });
 
 module.exports = router;
